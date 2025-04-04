8170c393c2975788d517f39bd3f7ee5d54a98367
Porshen Lai <porshenlai@gmail.com>
2025-04-05 01:55:26 +0800

    add logo page

--- docs/tab/Welcome.js
+++ docs/tab/Welcome.js
@@ -1,3 +1,20 @@
 document.currentScript.value=async (root,args)=>{
-	console.log("Script running...");
+	if(root.onClickHandler) root.removeEventListener("click",root.onClickHandler);
+	root.addEventListener("click",root.onClickHandler=function(event){
+		try {
+			switch(Piers.DOM(event.target).find("[func]").getAttribute("func")){
+			case "GoP1":
+				root.querySelector('[UID="P1"]').scrollIntoView({behavior:'smooth', block:'center'});
+				break;
+			case "GoP2":
+				root.querySelector('[UID="P2"]').scrollIntoView({behavior:'smooth', block:'center'});
+				break;
+			case "GoApp":
+				document.body.querySelector('[func="FuturePeek"]').click();
+				break;
+			}
+		} catch(x) { }
+		event.preventDefault();
+		event.stopPropagation();
+	});
 };
